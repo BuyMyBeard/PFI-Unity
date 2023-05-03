317908a449dd8838a22482d7fe8b05c7bec95fe1
JohnP55 <62863085+JohnP55@users.noreply.github.com>
2023-05-03 11:26:17 -0400

    what

---
+++ Assets/Scripts/GroundedCharacter.cs
@@ -0,0 +1,174 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public enum GroundedCharacterAnimations { Idle, Walking, Jumping, Raising, Falling, Landing }
+public enum Slope { Down, Up, None };
+
+public abstract class GroundedCharacter : MonoBehaviour
+{
+    [SerializeField] protected float terminalVelocity = -6;
+    [SerializeField] protected float gravAcceleration = -15;
+    [SerializeField] public float horizontalSpeed = 7;
+    [SerializeField] protected float jumpVelocity = 12;
+    [SerializeField] protected float groundCheckDistance = 0.1f;
+    [SerializeField] protected float compensation;
+    [SerializeField] protected LayerMask groundLayer;
+    [SerializeField] protected LayerMask platformLayer;
+    [SerializeField] protected PhysicsMaterial2D noFriction;
+    [SerializeField] protected PhysicsMaterial2D highFriction;
+    [SerializeField] protected float SlopeUpCompensation = 0.05f;
+    //AudioManagerComponent audioManager;
+
+    protected bool isTouchingGround, isTouchingPlatform;
+    protected Animator animator;
+    public Slope slope = Slope.None;
+    public SpriteRenderer Sprite { get; set; }
+    public Rigidbody2D RB { get; protected set; }
+    public CapsuleCollider2D CC { get; protected set; }
+    public Vector2 ColliderSize { get; protected set; }
+
+    public Vector2 Velocity
+    {
+        get => RB.velocity;
+        protected set => RB.velocity = value;
+    }
+
+    protected Vector2 newVelocity;
+
+    public bool IsFalling
+    {
+        get => !IsGrounded && Velocity.y < 0;
+    }
+    public bool IsJumping
+    {
+        get => !IsGrounded && Velocity.y > 0;
+    }
+    public bool IsGrounded
+    {
+        get => isTouchingGround || isTouchingPlatform;
+    }
+    public bool IsTouchingSlope
+    {
+        get => slope != Slope.None;
+    }
+
+    public bool IsWalking
+    {
+        get => IsGrounded && Velocity.x != 0;
+    }
+
+    protected void Awake()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        CC = GetComponent<CapsuleCollider2D>();
+        Sprite = GetComponent<SpriteRenderer>();
+        ColliderSize = CC.size * transform.localScale;
+        //audioManager = GetComponent<AudioManagerComponent>();
+        animator = GetComponent<Animator>();
+    }
+
+    protected void FixedUpdate()
+    {
+        newVelocity = Velocity;
+        FloorCheck();
+        AddGravity();
+        LimitVelocity();
+        Velocity = newVelocity;
+    }
+
+    protected virtual void LimitVelocity()
+    {
+        if (newVelocity.y < terminalVelocity)
+            newVelocity.y = terminalVelocity;
+    }
+
+    protected virtual void AddGravity()
+    {
+        if (IsGrounded)
+        {
+            newVelocity.y = 0;
+            RB.sharedMaterial = highFriction;
+        }
+        else
+        {
+            newVelocity.y += gravAcceleration * Time.deltaTime;
+            RB.sharedMaterial = noFriction;
+        }
+    }
+
+    protected virtual void AddSlopeCompensation()
+    {
+        if (slope == Slope.Up)
+            newVelocity.y = newVelocity.x;
+        else if (slope == Slope.Down)
+            newVelocity.y = -newVelocity.x;
+    }
+
+    protected virtual void FloorCheck()
+    {
+        Vector2 rayOrigin = transform.position - new Vector3(0, ColliderSize.y / 2) + (Vector3)(CC.offset * transform.localScale);
+        SlopeCheck(rayOrigin);
+        GroundCheck(rayOrigin);
+    }
+    protected void GroundCheck(Vector2 rayOrigin)
+    {
+        bool wasGrounded = IsGrounded;
+        RaycastHit2D groundHit = Physics2D.Raycast(rayOrigin, Vector2.down, groundCheckDistance, groundLayer);
+        RaycastHit2D platformHit = Physics2D.Raycast(rayOrigin, Vector2.down, groundCheckDistance, platformLayer);
+        isTouchingGround = !IsJumping && groundHit;
+        isTouchingPlatform = !IsJumping && platformHit;
+
+        if (!wasGrounded && IsGrounded)
+        {
+            if (isTouchingGround)
+                transform.Translate(0, -groundHit.distance, 0);
+            else
+                transform.Translate(0, -platformHit.distance, 0);
+        }
+        else if (!IsGrounded)
+            slope = Slope.None;
+        //supposed to compensate for moving up slope, but breaks a bunch of stuff
+        //else if (isTouchingGround && slope == Slope.None && groundHit.distance != 0)
+        //    transform.Translate(0, -groundHit.distance, 0);
+
+        Debug.DrawRay(rayOrigin, Vector2.down * groundCheckDistance, Color.red);
+    }
+    private void SlopeCheck(Vector2 rayOrigin)
+    {
+        Slope oldSlope = slope;
+        RaycastHit2D slopeHitRight = Physics2D.Raycast(rayOrigin, Vector2.right, groundCheckDistance, groundLayer);
+        RaycastHit2D slopeHitLeft = Physics2D.Raycast(rayOrigin, Vector2.left, groundCheckDistance, groundLayer);
+        if (!slopeHitLeft && !slopeHitRight)
+            slope = Slope.None;
+        else if (slopeHitLeft && slopeHitRight)
+        {
+            slope = Slope.None;
+            transform.Translate(0, compensation, 0);
+        }
+        else if (slopeHitRight)
+            slope = Slope.Up;
+        else //slopeHitBack
+            slope = Slope.Down;
+
+        if (oldSlope != Slope.None && slope == Slope.None && IsGrounded)
+            StartCoroutine(CompensateForSlopeUp());
+        //if (this is AngryRatComponent)
+        //{
+        //    Debug.Log($"({rayOrigin.x}, {rayOrigin.y}), {groundCheckDistance}");
+        //}
+        Debug.DrawRay(rayOrigin, Vector2.right * groundCheckDistance, Color.green);
+        Debug.DrawRay(rayOrigin, Vector2.left * groundCheckDistance, Color.blue);
+    }
+
+    IEnumerator CompensateForSlopeUp()
+    {
+        yield return new WaitForSeconds(SlopeUpCompensation);
+        Vector2 rayOrigin = transform.position - new Vector3(0, ColliderSize.y / 2) + (Vector3)(CC.offset * transform.localScale);
+        RaycastHit2D groundHit = Physics2D.Raycast(rayOrigin, Vector2.down, groundCheckDistance, groundLayer);
+        if (groundHit && IsGrounded && slope == Slope.None)
+        {
+            transform.Translate(0, -groundHit.distance, 0);
+        }
+    }
+}
--- Assets/Scripts/Player Scripts/PlayerMove.cs
+++ Assets/Scripts/Player Scripts/PlayerMove.cs
@@ -1,18 +1,158 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewBehaviourScript : MonoBehaviour
+[RequireComponent(typeof(PlayerInputComponent))]
+public class PlayerMove : GroundedCharacter
 {
-    // Start is called before the first frame update
-    void Start()
+    enum Animations { Stunned, Idle, Running, Jumping, Launch, Raising, Falling, Land };
+    [SerializeField] float ascendingDrag = 1;
+    [SerializeField] float holdingJumpDrag = 1;
+    [SerializeField] float coyoteTime = 0.2f;
+    [SerializeField] float stunnedGravityScale = 1;
+    [SerializeField] PhysicsMaterial2D ragdollPhysics;
+    //AudioManagerComponent sfx;
+    AudioSource audioSource;
+    Animations currentAnimation = Animations.Idle;
+
+    PlayerInputComponent inputs;
+    public bool inDeathPit = false;
+    bool isDroppingPlatform = false, stunned = false;
+    bool ended = false;
+    float coyoteTimeElapsed = 0;
+
+    public bool IsCoyoteTime
     {
-        
+        get => coyoteTimeElapsed < coyoteTime;
     }
 
-    // Update is called once per frame
-    void Update()
+    new private void Awake()
     {
-        
+        base.Awake();
+        audioSource = GetComponent<AudioSource>();
+        inputs = GetComponent<PlayerInputComponent>();
+        //sfx = GetComponent<AudioManagerComponent>();
+    }
+
+    new private void FixedUpdate()
+    {
+        if (stunned || ended)
+            return;
+
+        newVelocity = Velocity;
+        FloorCheck();
+        SetHorizontalVelocity();
+        AddGravity();
+        AddSlopeCompensation();
+        CheckInputs();
+        AddDrag();
+        LimitVelocity();
+        Velocity = newVelocity;
+        if (IsGrounded)
+            ResetCoyoteTime();
+    }
+
+    private void SetAnimation(Animations animation)
+    {
+        //if (currentAnimation != animation)
+        //{
+        //    if (animation == Animations.Running)
+        //        audioSource.Play();
+        //    else
+        //        audioSource.Stop();
+
+        //    animator.Play(dictAnimations[animation]);
+        //    currentAnimation = animation;
+        //}
+    }
+
+
+    private void SetHorizontalVelocity()
+    {
+        if (inDeathPit)
+        {
+            newVelocity.x = 0;
+            return;
+        }
+        newVelocity.x = inputs.MoveInput.x * horizontalSpeed;
+        if (inputs.MoveInput.x != 0)
+            Sprite.flipX = inputs.MoveInput.x > 0;
+    }
+
+    // TODO: Update
+
+    private void CheckInputs()
+    {
+        if (inputs.JumpPressInput && (IsGrounded || IsCoyoteTime) && !IsJumping && !inDeathPit)
+            newVelocity.y = jumpVelocity;
+
+    }
+
+    protected override void AddGravity()
+    {
+        // TODO: Decide if we use this or not
+        //if (inputs.DropInput && isTouchingPlatform && !isDroppingPlatform)
+        //{
+        //    StartCoroutine(DropPlatform());
+        //}
+        //if (isDroppingPlatform)
+        //    isTouchingPlatform = false;
+        if (IsGrounded)
+        {
+            newVelocity.y = 0;
+            RB.sharedMaterial = highFriction;
+        }
+        else
+        {
+            newVelocity.y += gravAcceleration * Time.deltaTime;
+            coyoteTimeElapsed += Time.deltaTime;
+            RB.sharedMaterial = noFriction;
+        }
+    }
+
+    IEnumerator DropPlatform()
+    {
+        coyoteTimeElapsed += coyoteTime;
+        newVelocity.y = 0;
+        isDroppingPlatform = true;
+        yield return new WaitForSeconds(0.1f);
+        isDroppingPlatform = false;
+    }
+
+    private void AddDrag()
+    {
+        if (IsJumping)
+        {
+            newVelocity.y += ascendingDrag * Time.deltaTime;
+            if (inputs.JumpHoldInput)
+                newVelocity.y += holdingJumpDrag * Time.deltaTime;
+        }
+    }
+
+    public void ResetCoyoteTime()
+    {
+        coyoteTimeElapsed = 0;
+    }
+    public void TakeKnockBack(Vector2 knockback)
+    {
+        SetAnimation(Animations.Stunned);
+        currentAnimation = Animations.Stunned;
+        RB.velocity = knockback;
+        RB.gravityScale = stunnedGravityScale;
+        stunned = true;
+        RB.sharedMaterial = ragdollPhysics;
+        //sfx.PlaySFX(0);
+    }
+    public void Recover()
+    {
+        currentAnimation = Animations.Idle;
+        SetAnimation(Animations.Idle);
+        RB.gravityScale = 0;
+        stunned = false;
     }
 }

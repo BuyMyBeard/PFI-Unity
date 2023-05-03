72ca77a674f28c082853f972312c5632ce28ff55
Alexandre Lacombe <201524011@edu.clg.qc.ca>
2023-05-03 09:57:16 -0400

    added player inputs

---
+++ Assets/Scripts/Player Scripts/PlayerInput.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputComponent : MonoBehaviour
+{
+    private void Awake()
+    {
+        InputMap inputMap = new InputMap();
+
+    }
+}

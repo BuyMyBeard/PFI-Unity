3d4314d5975a3568b077f93f743ff1d0c4f6f0d4
Afernu <cedricmnpierre@hotmail.com>
2023-05-10 12:03:52 -0400

    Cutscene de debut

--- Assets/StartCutscene.cs
+++ Assets/StartCutscene.cs
@@ -1,18 +1,44 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class StartCutscene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    GameObject player;
+    CinemachineVirtualCamera vCamera;
+    GameObject lava;
+    private Vector2 direction = Vector2.down;
+    [SerializeField] float speed = 5f;
+    [SerializeField] float time = 5f;
+
+
+    private void Awake()
     {
-        
+        player = FindObjectOfType<PlayerMove>().gameObject;
+        vCamera = FindObjectOfType<CinemachineVirtualCamera>();
+        lava = FindObjectOfType<RisingLava>().gameObject;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
+        StartCoroutine(BeginCutscene(time));
+    }
+    private void Update()
+    {
+        if (!(transform.position.y < -4))
+            transform.Translate(speed * Time.deltaTime * direction);
+    }
+    public IEnumerator BeginCutscene(float time)
+    {
+        lava.SetActive(false);
+        vCamera.Follow = transform;
+        yield return new WaitForSeconds(time);
+        vCamera.Follow = player.transform;
+        lava.SetActive(true);
+        gameObject.SetActive(false);
         
     }
 }
